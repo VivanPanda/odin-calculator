31cb8b31331cc38e7d495f284c1f2f52175b30c0
Vivan <vivanpanda13@gmail.com>
2023-05-12 10:59:20 +0400

    Acccess all buttons using dom method querySelector

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 // get dom properties
 
+const upperText = document.querySelector('.upper-display-text');
+const resultText = document.querySelector('.result-text');
+const acButton = document.querySelector('#ac-button');
+const ceButton = document.querySelector('#ce-button');
+
+const divisionButton = document.querySelector('#division-button');
+const multiplicationButton = document.querySelector('#multiplication-button');
+const subtractionButton = document.querySelector('#subtraction-button');
+const additionButton = document.querySelector('#addition-button');
+const dotButton = document.querySelector('#decimal-button');
+const equalButton = document.querySelector('#equal-button');
+
+const number0Button = document.querySelector('#number-0-button');
+const number1Button = document.querySelector('#number-1-button');
+const number2Button = document.querySelector('#number-2-button');
+const number3Button = document.querySelector('#number-3-button');
+const number4Button = document.querySelector('#number-4-button');
+const number5Button = document.querySelector('#number-5-button');
+const number6Button = document.querySelector('#number-6-button');
+const number7Button = document.querySelector('#number-7-button');
+const number8Button = document.querySelector('#number-8-button');
+const number9Button = document.querySelector('#number-9-button');
 
 let runningTotal = 0;
 
